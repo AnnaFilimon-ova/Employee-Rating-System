d7067ab5ade33887adca673bbc38ba8c75294351
AnnaFilimon-ova <annafilimon.ova6666@gmail.com>
2026-09-12 20:22:26 +0300

    hash password

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 from database import connect
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+import bcrypt
 
 app = FastAPI()
 
 #We see a list of managers.
 @app.get("/")
-def Get_managers():
+def get_managers():
     connection = connect()
     cursor = connection.cursor()
 
-    cursor.execute("SELECT * FROM users")
+    cursor.execute("SELECT id, name, status FROM users")
     users = cursor.fetchall()
 
     cursor.close()
@@ -20,57 +21,86 @@ def Get_managers():
         {
             "id": user[0],
             "name": user[1],
-            "password": user[2],
             "status": user[3]
         }
         for user in users
     ]
 
 class User(BaseModel):
-    name: str
-    password: str
+    name: str = Field(min_length=1, max_length=50)
+    password: str = Field(min_length=9, max_length=50)
 
 #Manager sign up
 @app.post("/users")
-def Register_manager(user: User):
+def register_manager(user: User):
     connection = connect()
     cursor = connection.cursor()
-    cursor.execute(
-        """
-        INSERT INTO users (name, password)
-        VALUES (%s, %s)
-        """,
-        (user.name, user.password)
-    )
-    connection.commit()
-    cursor.close()
-    connection.close()
 
-    return {"message": "User added successfully"}
+    try:
+        hashed_password = bcrypt.hashpw(
+            user.password.encode("utf-8"),
+            bcrypt.gensalt()
+        ).decode("utf-8")
+        cursor.execute(
+            """
+            INSERT INTO users (name, password)
+            VALUES (%s, %s)
+            """,
+            (user.name, user.password)
+        )
+
+        manager = cursor.fetchone()
+        connection.commit()
+        return {
+            "message": "Registration is successfully",
+            "id": manager[0],
+            "name": manager[1],
+            "status": manager[2]
+        }
+
+    except Exception:
+        connection.rollback()
+        raise
+
+    finally:
+        cursor.close()
+        connection.close()
 
 #Manager sign in
 @app.post("/login")
-def Login_manager(user: User):
+def login_manager(user: User):
     connection = connect()
     cursor = connection.cursor()
-    cursor.execute(
-        """
-        select id, name, status from users
-        where name = %s and password = %s
-        """,
-        (user.name, user.password)
-    )
 
-    manager = cursor.fetchone()
-    cursor.close()
-    connection.close()
+    try:
+        cursor.execute(
+            """
+            select id, name, status from users
+            where name = %s and password = %s
+            """,
+            (user.name, user.password)
+        )
 
-    if manager is None:
-        return {"message": "User not found"}
+        manager = cursor.fetchone()
 
-    return {
-        "message": "Logged in successfully",
-        "id": manager[0],
-        "name": manager[1],
-        "status": manager[2]
-    }
+        if manager is None:
+            return {"message": "User not found"}
+
+        password_correct = bcrypt.checkpw(
+            user.password.encode("utf-8"),
+            manager[2].encode("utf-8")
+        )
+
+        if not password_correct:
+            return {"message": "Wrong password"}
+
+        return {
+            "message": "Logged in successfully",
+            "id": manager[0],
+            "name": manager[1],
+            "status": manager[2]
+        }
+
+    finally:
+        cursor.close()
+        connection.close()
